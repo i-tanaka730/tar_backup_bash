4625faca5dfed1e4d626e5013960b66f7c259301
Ikuya Tanaka <41749871+i-tanaka730@users.noreply.github.com>
2021-04-20 09:05:22 +0900

    Create tar_backup.sh

---
+++ tar_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# @filename tar_backup.sh
+
+# バックアップ先
+BAKDIR="/var/www/backup";
+
+# バックアップファイル名 (ex: hoge_20210415.tar.gz)
+BACKFILE="hoge_`date +%Y%m%d`.tar.gz"
+
+# バックアップ元
+TARGET="var/www/hoge";
+
+# バックアップ実行
+cd /;
+tar -czf ${BAKDIR}/${BACKFILE} ${TARGET} 1> /dev/null;
